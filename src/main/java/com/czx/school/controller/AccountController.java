338e49cc3fda14f869be6fba0a92032aa4f8adad
ShuanZi <1373833569@qq.com>
2025-10-19 01:33:18 +0800

    解决了跨域问题和前后端数据映射问题@RequestBody

--- src/main/java/com/czx/school/controller/AccountController.java
+++ src/main/java/com/czx/school/controller/AccountController.java
@@ -6,6 +6,7 @@ import com.czx.school.VO.Response;
 import com.czx.school.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,18 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
     @Autowired
     private AccountService accountService;
+
     @PostMapping("/login")
-    public Response<String> login(LoginDTO loginDTO){
-        if(accountService.selectByUsername(loginDTO.getUsername())== null){
+    public Response<String> login(@RequestBody LoginDTO loginDTO) {
+        if (accountService.selectByUsername(loginDTO.getUsername()) == null) {
             return Response.error("1001", "用户不存在，请先注册");
         }
-        return accountService.login(loginDTO)? Response.success("登录成功"): Response.error("1002", "登录失败，用户名或密码错误");
+        return accountService.login(loginDTO) ? Response.success("登录成功") : Response.error("1002", "登录失败，用户名或密码错误");
     }
+
     @PostMapping("/register")
-    public Response<String> register(RegisterDTO registerDTO){
-        if(accountService.selectByUsername(registerDTO.getUsername()) != null){
+    public Response<String> register(@RequestBody RegisterDTO registerDTO) {
+        if (accountService.selectByUsername(registerDTO.getUsername()) != null) {
             return Response.error("1003", "用户已存在");
         }
-        return accountService.register(registerDTO)? Response.success("注册成功"): Response.error("1004", "注册失败");
+        return accountService.register(registerDTO) ? Response.success("注册成功") : Response.error("1004", "注册失败");
     }
 }
+
